2e9d8b6dda5755968b253139dbd6fca64b63335d
= <=>
2012-11-01 16:44:22 +0800

    Implemented basic_mongodb.py, subscribing dionaea.connections and geoloc.evnets.

--- cli/basic_mongodb.py
+++ cli/basic_mongodb.py
@@ -5,19 +5,61 @@ import logging
 logging.basicConfig(level=logging.WARNING)
 
 import hpfeeds
+import pymongo
+import ast
+import datetime
 
 HOST = '192.168.168.113'
 PORT = 10000
-CHANNELS = ['dionaea.shellcodeprofiles', 'dionaea.capture', 'thug.events', ]
+CHANNELS = ['dionaea.connections', 'geoloc.events']
 IDENT = 'ident'
 SECRET = 'secret'
+
+# Required
+MONGOHOST = '127.0.0.1'
+MONGOPORT = 27017
+MONGODBNAME = 'hpfeeds-test'
+# Optional
+MONGOUSER = ''
+MONGOPWD = ''
+
+def get_db(host, port, name, user = '', passwd = ''):
+        dbconn = pymongo.Connection(host, port)
+        db = pymongo.database.Database(dbconn, name)
+	if user != '' or passwd != '':
+        	db.authenticate(user, passwd)
+        return db
+
 
 def main():
 	hpc = hpfeeds.new(HOST, PORT, IDENT, SECRET)
 	print >>sys.stderr, 'connected to', hpc.brokername
 
+	db = get_db(MONGOHOST, MONGOPORT, MONGODBNAME, MONGOUSER, MONGOPWD)
+
 	def on_message(identifier, channel, payload):
-		print 'msg', identifier, channel, payload
+		if channel == 'dionaea.connections':
+			try:
+				msg = ast.literal_eval(str(payload))
+			except:
+				print 'exception processing dionaea.connections event', repr(payload)
+			else:
+				msg["time"] = datetime.datetime.utcfromtimestamp(msg['time'])
+				msg['rport'] = int(msg['rport'])
+				msg['lport'] = int(msg['lport'])
+				print 'inserting...', msg
+				db['dionaea'].insert(msg)
+		elif channel == 'geoloc.events':
+			try:
+				payload_python = str(payload)
+				msg = ast.literal_eval(payload_python.replace("null", "None"))
+			except:
+				print 'exception processing geoloc.events', repr(payload)
+			else:
+				msg['time'] = datetime.datetime.strptime(msg['time'], "%Y-%m-%d %H:%M:%S")
+				print 'inserting...', msg
+				db['geoloc'].insert(msg)
+
 
 	def on_error(payload):
 		print >>sys.stderr, ' -> errormessage from server: {0}'.format(payload)
