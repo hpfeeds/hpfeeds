788a2220b88feb92200593b9af421c83232f5fda
chenjj <whucjj@gmail.com>
2012-11-01 06:38:58 -0700

    store the data which is from dionaea.capture mwbinary.dionaea.sensorunique dionaea.shellcodeprofiles dionaea.dcerpcrequests  channels to mongodb

--- cli/basic_mongodb.py
+++ cli/basic_mongodb.py
@@ -3,22 +3,23 @@
 import sys
 import logging
 logging.basicConfig(level=logging.WARNING)
-
+from  gridfs  import GridFS
 import hpfeeds
 import pymongo
 import ast
 import datetime
+import md5
 
-HOST = '192.168.168.113'
+HOST = '127.0.0.1'
 PORT = 10000
-CHANNELS = ['dionaea.connections', 'geoloc.events']
-IDENT = 'ident'
-SECRET = 'secret'
+CHANNELS = ['dionaea.connections', 'geoloc.events','dionaea.dcerpcrequests','dionaea.shellcodeprofiles','mwbinary.dionaea.sensorunique','dionaea.capture']
+IDENT = 'ww3ee@hp1'
+SECRET = '7w35rippuhx7704h'
 
 # Required
 MONGOHOST = '127.0.0.1'
 MONGOPORT = 27017
-MONGODBNAME = 'hpfeeds-test'
+MONGODBNAME = 'dionaea'
 # Optional
 MONGOUSER = ''
 MONGOPWD = ''
@@ -35,8 +36,10 @@ def main():
 	hpc = hpfeeds.new(HOST, PORT, IDENT, SECRET)
 	print >>sys.stderr, 'connected to', hpc.brokername
 
-	db = get_db(MONGOHOST, MONGOPORT, MONGODBNAME, MONGOUSER, MONGOPWD)
-
+	insertCon = pymongo.Connection(host="localhost",port=27017)
+	db = None
+	collection = None
+	
 	def on_message(identifier, channel, payload):
 		if channel == 'dionaea.connections':
 			try:
@@ -48,7 +51,9 @@ def main():
 				msg['rport'] = int(msg['rport'])
 				msg['lport'] = int(msg['lport'])
 				print 'inserting...', msg
-				db['dionaea'].insert(msg)
+				db = insertCon['dionaea']
+				collection = db['connection']
+				collection.insert(msg)
 		elif channel == 'geoloc.events':
 			try:
 				payload_python = str(payload)
@@ -58,9 +63,62 @@ def main():
 			else:
 				msg['time'] = datetime.datetime.strptime(msg['time'], "%Y-%m-%d %H:%M:%S")
 				print 'inserting...', msg
-				db['geoloc'].insert(msg)
-
-
+				db = insertCon['geoloc']
+				collection =  db['events']
+				collection.insert(msg)
+		elif channel == 'dionaea.dcerpcrequests':
+			try:
+				payload_python = str(payload)
+				msg = ast.literal_eval(payload_python.replace("null", "None"))
+			except:
+				print 'exception processing dionaea.dcerpcrequests', repr(payload)
+			else:
+				dt = datetime.datetime.now()
+				msg['time'] = dt.strftime('%Y-%m-%d %H:%M:%S')
+				print 'inserting...', msg
+				db = insertCon['dionaea']
+				collection = db['dcerpcrequests']
+				collection.insert(msg)
+		elif channel == 'dionaea.shellcodeprofiles':
+			try:
+				payload_python = str(payload)
+				msg = ast.literal_eval(payload_python.replace("null", "None"))
+			except:
+				print 'exception processing dionaea.shellcodeprofiles', repr(payload)
+			else:
+				dt = datetime.datetime.now()
+				msg['time'] = dt.strftime('%Y-%m-%d %H:%M:%S')
+				print 'inserting...', msg
+				db = insertCon['dionaea']
+				collection = db['shellcodeprofiles']
+				collection.insert(msg)
+		elif channel == 'mwbinary.dionaea.sensorunique' :
+			try:
+				payload_python = str(payload)
+			except:
+				print 'exception processing mwbinary.dionaea.sensorunique', repr(payload)
+			else:
+				hash = md5.new()
+				hash.update(payload_python)
+				msg = hash.hexdigest()
+				print 'inserting mwbinary...', msg
+				
+				db = insertCon['dionaea']
+				gfsDate=GridFS(db)
+				gfsDate.put(payload_python,filename=msg)
+		elif channel == 'dionaea.capture':
+			try:
+				payload_python = str(payload)
+				msg = ast.literal_eval(payload_python.replace("null", "None"))
+			except:
+				print 'exception processing dionaea.capture', repr(payload)
+			else:
+				dt = datetime.datetime.now()
+				msg['time'] = dt.strftime('%Y-%m-%d %H:%M:%S')
+				print 'inserting...', msg
+				db = insertCon['dionaea']
+				collection = db['capture']
+				collection.insert(msg)		
 	def on_error(payload):
 		print >>sys.stderr, ' -> errormessage from server: {0}'.format(payload)
 		hpc.stop()
