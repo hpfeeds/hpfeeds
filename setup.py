1767412ac9218f2570e66c2c5b9b62daa0d58dd3
John Carr <john.carr@unrouted.co.uk>
2018-06-02 09:46:25 +0100

    Back to development: 0.9.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='hpfeeds3',
-    version='0.9.4',
+    version='0.9.5.dev0',
     description='Python implementation of the honeypot feeds broker',
     author='John Carr',
     author_email='john.carr@unrouted.co.uk',
