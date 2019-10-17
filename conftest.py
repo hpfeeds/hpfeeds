04f41d219fde366b431efbf75a57e0a13881799c
John Carr <john.carr@unrouted.co.uk>
2019-10-17 12:39:29 +0100

    Ignore py3 only tests on py2

--- conftest.py
+++ conftest.py
@@ -10,5 +10,7 @@ if sys.version_info[0] == 2:
         "hpfeeds/tests/test_broker_prometheus.py",
         "hpfeeds/tests/test_client_integration.py",
         "hpfeeds/tests/test_twisted_service.py",
+        "hpfeeds/tests/test_broker_auth_env.py",
+        "hpfeeds/tests/test_broker_auth_sqlite.py",
     ])
 
