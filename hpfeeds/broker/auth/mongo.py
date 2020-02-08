2437ec4b9c73dab4ddd76aef981336e075920458
KevTheHermit <kevthehermit@gmail.com>
2020-02-08 11:57:51 +0000

    Fix io_loop wrong loop

--- hpfeeds/broker/auth/mongo.py
+++ hpfeeds/broker/auth/mongo.py
@@ -14,18 +14,22 @@ from hpfeeds.broker.server import ServerException
 class Authenticator(object):
 
     def __init__(self, connection_string):
-
-        # we need to wait for the loop
-        loop = asyncio.get_event_loop()
-
         self.logger = logging.getLogger('hpfeeds.broker.auth.mongo.Authenticator')
-        self.logger.info('Creating Authenticator Class')
+        self.connection = False
         try:
-            dbstring, dbname = connection_string.rsplit('/', 1)
-            self.client = motor.motor_asyncio.AsyncIOMotorClient(dbstring, io_loop=loop)
-            self.db = self.client[dbname]
+            self.dbstring, self.dbname = connection_string.rsplit('/', 1)
+        except Exception as err:
+            ServerException("Unable to create connection string: {0}".format(err))
+
+    async def db_connect(self):
+        try:
+            self.logger.debug("Connecting to Mongo DB")
+            loop = asyncio.get_event_loop()
+            self.client = motor.motor_asyncio.AsyncIOMotorClient(self.dbstring, io_loop=loop)
+            self.db = self.client[self.dbname]
             self.collection = self.db['auth_key']
         except Exception as err:
+            self.connection = False
             ServerException("Unable to connect to mongo database: {0}".format(err))
 
     async def start(self):
@@ -37,6 +41,13 @@ class Authenticator(object):
 
     async def get_authkey(self, ident):
         self.logger.debug("Auth key for {0} requested".format(ident))
+
+        # We need to wait for the asyncio loop to be up
+        # So set everything up on the first auth call at which point we should be running.
+        if not self.connection:
+            await self.db_connect()
+            self.connection = True
+
         auth_key = await self.collection.find_one({"identifier": ident})
 
         if not auth_key:
