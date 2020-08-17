2ca4b7a9271b3b9f46a8210050e0500d0b0bbef4
John Carr <john.carr@unrouted.co.uk>
2020-08-17 12:15:35 +0100

    Fix tests on 2.7

--- conftest.py
+++ conftest.py
@@ -4,13 +4,13 @@ import sys
 collect_ignore = []
 if sys.version_info[0] == 2:
     collect_ignore.extend([
-        "hpfeeds/tests/test_asyncio_client.py",
-        "hpfeeds/tests/test_asyncio_protocol.py",
-        "hpfeeds/tests/test_broker_connection.py",
-        "hpfeeds/tests/test_broker_prometheus.py",
-        "hpfeeds/tests/test_client_integration.py",
-        "hpfeeds/tests/test_twisted_service.py",
-        "hpfeeds/tests/test_broker_auth_env.py",
-        "hpfeeds/tests/test_broker_auth_sqlite.py",
+        "tests/test_asyncio_client.py",
+        "tests/test_asyncio_protocol.py",
+        "tests/test_broker_connection.py",
+        "tests/test_broker_prometheus.py",
+        "tests/test_client_integration.py",
+        "tests/test_twisted_service.py",
+        "tests/test_broker_auth_env.py",
+        "tests/test_broker_auth_sqlite.py",
     ])
 
